dcaabc6dad50adf700ef7e863a1551b272b299e5
Icode-designs <i.coder.6969@gmail.com>
2024-09-28 14:05:01 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
